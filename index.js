53b8162f49b5fd0518362f9b5fcabe246b1fdf9e
Hinkley <Se.G.Hinkley@baypath.net>
2025-02-11 13:20:38 -0500

    Rooms

--- index.js
+++ index.js
@@ -19,11 +19,23 @@ io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
     socket.on('chat message', (data) => {
         io.emit('chat message', {user: socket.username, message: data});
-    })
+    });
 
     socket.on('username', (data) => {
         socket.username = data;
-    })
+    });
+
+    socket.on('createRoom', (data) => {
+        socket.currentRoom = data.room;
+        socket.join(data.room);
+        io.to(data.room).emit('roomCreated', {user: socket.username, room: data.room});
+    });
+
+    
+    socket.on('messageRoom', (data) => {
+        console.log(socket.currentRoom);
+        io.to(socket.currentRoom).emit("roomMessage", {user: socket.username, message: data.message})
+    });
 });
 
 server.listen(3000, () => {
